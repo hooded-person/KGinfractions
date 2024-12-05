74f06d54aa0e22aa961e973117c7338a20c0b481
Niels Groen <hooded.main@gmail.com>
2024-12-05 22:51:26 +0100

    more functions

--- setup/installer.lua
+++ setup/installer.lua
@@ -16,12 +16,12 @@ local function softError(err)
 end
 
 local function getUrl(url)
-    local canRequest, err = http.checkURL(pgrmFilesURL);
+    local canRequest, err = http.checkURL(url);
     if not canRequest then
         error(err);
     end;
     local response, err, failResponse = http.get({
-        url = pgrmFilesURL,
+        url = url,
     });
     if err then
         response = failResponse;
@@ -41,40 +41,36 @@ local function getUrl(url)
     };
 end
 
+
+---@param url string the url to get json data from
+---@return table unserialised JSON data
+---@return table full response data
+local function getJsonData(url)
+    local success, responseData = getUrl(url)
+    local statusCode = responseData.statusCode
+    local headers = responseData.headers
+    local body = responseData.body
+
+    assert(headers["Content-Type"] == "text/plain; charset=utf-8", 
+        "unexpected content type,\nResponse header 'Content-Type' did not match 'text/plain; charset=utf-8'"
+        );
+
+    local jsonData = textutils.unserialiseJSON(body);
+    assert(prgmFiles ~= nil, "failed too unserialise response file");
+    return jsonData, responseData
+end
+
 local function downloadFile(url)
     local success, responseData = getUrl(url)
     
 end
 
-local canRequest, err = http.checkURL(pgrmFilesURL);
-if not canRequest then
-    error(err);
-end;
-local response, err, failResponse = http.get({
-    url = pgrmFilesURL,
-});
-if err then
-    response = failResponse;
-end;
-local statusCode = response.getResponseCode();
-local headers = response.getResponseHeaders();
-local body = response.readAll();
-response.close();
-if err then -- show http errors (will be double up with github, ex. "HTTP 404\n 404: not found")
-    local errMsg = "HTTP "..statusCode.."\n"..body;
-    error(errMsg);
-end;
+local prgmFiles = getJsonData(pgrmFilesURL)
 
-assert(headers["Content-Type"] == "text/plain; charset=utf-8", 
-    "unexpected content type,\nResponse header 'Content-Type' did not match 'text/plain; charset=utf-8'"
-    );
-
-local prgmFiles = textutils.unserialiseJSON(body);
-assert(prgmFiles ~= nil, "failed too unserialise response file");
-
+-- configure a project root
 local success = false
 local projectRoot
-while not success do
+repeat
     print("Enter filepath for program location (or 'abort' to abort)")
     projectRoot = read()
     if projectRoot == "" then softError("please input a value")
@@ -88,9 +84,24 @@ while not success do
         success = true
         projectRoot = projectRoot..(projectRoot:sub(-1)=="/" and "" or "/")
     end
-end
+until success
+settings.define("KGinfractions.root", {
+    description = "The program root",
+    default = "/",
+    type = "string"
+})
+settings.set("KGinfractions.root", projectRoot)
+settings.save()
 
-for directory in ipairs(prgmFiles.directories) do
-    local dirPath = projectRoot..directory
-    fs.makeDir(dirPath)
+local function installItems(directories, files)
+    for directory in ipairs(directories) do
+        local dirPath = settings.get("KGinfractions.root")..directory
+        fs.makeDir(dirPath)
+    end
+    for file in ipairs(files) do
+        downloadFile(fileLocation..file)
+    end
 end
+-- always install
+installItems(prgmFiles.directories, prgmFiles.files)
+-- handle optional modules/templates
